8b5936db198f5ba88e7a0e901196da387f7fcbff
Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2024-10-29 15:56:16 +0300

    fix: fix kind for short and longtext

--- DYN_FORM/script.js
+++ DYN_FORM/script.js
@@ -22,16 +22,16 @@ const formDef1=
   {caption:'Опубликовать',kind:'submit'},
 ];
 
-function createForm(formDef1) {
+function createForm(formDef) {
   const body = document.body
   let form = document.createElement('form')
-    for (let i = 0; i < formDef1.length; i++) {
+    for (let i = 0; i < formDef.length; i++) {
       let div = document.createElement('div')
       let label = document.createElement('label')
-      label.textContent = formDef1[i].label
-        if(formDef1[i].kind === 'dropdown') {
+      label.textContent = formDef[i].label
+        if(formDef[i].kind === 'dropdown') {
           let input = document.createElement('select')
-            formDef1[i].variants.forEach((item) => {
+            formDef[i].variants.forEach((item) => {
               let option = document.createElement('option');
               option.value = item.value
               option.textContent = item.text;
@@ -40,44 +40,58 @@ function createForm(formDef1) {
               div.appendChild(input)
             })
         }
-        else if (formDef1[i].kind === 'radio') {
+        else if (formDef[i].kind === 'radio') {
           div.appendChild(label)
-          formDef1[i].variants.forEach((item) => {
+          formDef[i].variants.forEach((item) => {
           let radioLabel = document.createElement('label')
           let radioInput = document.createElement('input')
           radioInput.type = 'radio'
           radioInput.value = item.value
-          radioInput.name = formDef1[i].name
+          radioInput.name = formDef[i].name
           radioLabel.textContent = item.text
           radioLabel.appendChild(radioInput)
           div.appendChild(radioLabel)
             })
-        } else if (formDef1[i].kind === 'check') {
+        } else if (formDef[i].kind === 'check') {
           input = document.createElement('input')
           input.type = 'checkbox'
           div.appendChild(label)
           div.appendChild(input)
-
-        } else if (formDef1[i].kind === 'memo') {
+        } else if (formDef[i].kind === 'memo') {
           input = document.createElement('textarea')
           input.name = formDef1[i].name
           div.appendChild(label)
           div.appendChild(input)
-        } else if (formDef1[i].kind === 'submit') {
+
+        } else if (formDef[i].kind === 'submit') {
           input = document.createElement('input')
-          input.type = formDef1[i].kind
-          input.value = formDef1[i].caption
+          input.type = 'submit'
+          input.value = formDef[i].caption
           div.appendChild(label)
           div.appendChild(input)
-          
-        } else {
+        } else if (formDef[i].kind === 'shorttext') {
           input = document.createElement('input')
-          input.type = formDef1[i].kind
-          input.name = formDef1[i].name
+          input.type = 'text'
+          input.name = formDef[i].name
+          input.style.width = '120px';
+          div.appendChild(label)
+          div.appendChild(input)
+        } else if (formDef[i].kind === 'longtext') {
+          input = document.createElement('input')
+          input.type = 'text'
+          input.name = formDef[i].name
+          input.style.width = '240px';
+          div.appendChild(label)
+          div.appendChild(input)
+        } else if (formDef[i].kind === 'number') {
+          input = document.createElement('input')
+          input.type = 'number'
+          input.name = formDef[i].name
+          input.style.width = '50px';
           div.appendChild(label)
           div.appendChild(input)
         }
-        let styles = {width:'350px', padding:'5px', display:'flex', justifyContent: 'space-between'}
+        let styles = {width:'380px', padding:'5px', display:'flex', justifyContent: 'space-between'}
         for (let key in styles) {
           div.style[key] = styles[key]
         }
