16cbdab03b37125605e6f318135dccc9b7adb1b4
Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2024-12-10 17:06:12 +0300

    Update script.js

--- Traffic_Lanes/script.js
+++ Traffic_Lanes/script.js
@@ -8,12 +8,13 @@ let pathsLengths = {}
 start.addEventListener('click', startGame)
 
 class Road {
-  constructor(fieldSVG, offsetX = 0, offsetY = 0) {
+  constructor(fieldSVG, offsetX = 0, offsetY = 0, angle) {
     this.fieldSVG = fieldSVG;
     this.offsetX = offsetX;
     this.offsetY = offsetY;
+    this.angle = angle
     this.group = document.createElementNS("http://www.w3.org/2000/svg", "g");
-    this.group.setAttribute('transform', `translate(${this.offsetX}, ${this.offsetY})`);
+    this.group.setAttribute('transform', `translate(${this.offsetX}, ${this.offsetY}) rotate(${this.angle})`);
     this.fieldSVG.appendChild(this.group);
   }
 
@@ -23,9 +24,18 @@ class Road {
     rect.setAttribute('y', y);
     rect.setAttribute('width', width);
     rect.setAttribute('height', height);
-    rect.setAttribute('transform', transform);
+    rect.setAttribute('transform', transform, `rotate(${this.angle})`);
     rect.setAttribute('fill', fill);
     this.group.appendChild(rect);
+  }
+
+  createPath(d, fill, stroke, strokeWidth) {
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute('d', d);
+    path.setAttribute('fill', fill);
+    path.setAttribute('stroke', stroke);
+    path.setAttribute('stroke-width', strokeWidth);
+    this.group.appendChild(path);
   }
 
   draw() {
@@ -38,35 +48,74 @@ class Road {
     this.createRect('2', '-4', '2', '100', 'rotate(90 0 96)', '#E5AE09');
   }
 
-  createPath(id, pathCoordinates) {
-    let route = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    route.setAttribute('id', id);
-    route.setAttribute('d', pathCoordinates);
-    route.setAttribute('stroke', 'black');
-    route.setAttribute('fill', 'transparent');
-    fieldSVG.appendChild(route);
-    let pathLength = route.getTotalLength();
-    pathsLengths[`#${id}`] = { length: pathLength, element: route }
-    console.log(pathsLengths);
+  drawCrossroads() {
+    this.createPath("M43.5588 40.059L54.5588 24.059L57.5588 5.05896H104.559L109.059 24.059L118.559 40.059L135.559 52.059L156.059 56.559V104.059L135.559 108.559L118.059 120.559L108.059 136.559L105.059 155.059H57.0588L54.0588 138.059L43.5588 120.559L27.5588 108.059L6.05884 104.059V56.059L27.0588 52.059L43.5588 40.059Z", "#D9D9D9", "none", "0");
+    this.createPath("M105.059 155.059C105.059 126.019 126.702 105.559 156.559 104.059", "none", "#E5AE09", "2");
+    this.createPath("M156.059 56.0588C127.019 56.0587 106.559 34.4154 105.059 4.55893", "none", "#E5AE09", "2");
+    this.createPath("M57.0587 5.05893C57.0586 34.0989 35.9153 54.5592 6.05884 56.059", "none", "#E5AE09", "2");
+    this.createPath("M6.05877 104.059C35.0987 104.059 55.5591 125.702 57.0588 155.559", "none", "#E5AE09", "2");
+    this.createRect("6", "81", "4", "22", "", "white");
+    this.createRect("152", "57", "4", "22", "", "white");
+    this.createRect("58", "9", "4", "22", "rotate(-90 58 9)", "white");
+    this.createRect("82", "155", "4", "22", "rotate(-90 82 155)", "white");
   }
 }
 
 let fieldSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+fieldSVG.style.backgroundColor = '#8fe577';
 fieldSVG.setAttribute('width', '870');
 fieldSVG.setAttribute('height', '690');
-fieldSVG.setAttribute('viewBox', '0 0 870 690');
-fieldSVG.setAttribute('fill', '#E50E09');
+fieldSVG.setAttribute('viewBox', '0 50 870 690');
 fieldSVG.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
 fieldSVG.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink');
 gameField.appendChild(fieldSVG);
 
-let n = 9;
-let offsetX = 0;
-for (let i = 0; i < n; i++) {
-  const roadPart = new Road(fieldSVG, offsetX, 0);
-  roadPart.draw(); // создание дороги
-  offsetX += 100;
+
+function createMap() {
+  let n = 4;
+  offsetX = -38;
+  offsetY = 20
+  for (let i = 0; i < n; i++) {
+    const roadPart = new Road(fieldSVG, offsetX, 320, 0);
+    roadPart.draw();
+    offsetX += 100;
+  }
+  let crossroads = new Road(fieldSVG, 355, 315, 0);
+  crossroads.drawCrossroads();
+  offsetX = 510
+  for (let i = 0; i < n; i++) {
+    const roadPart = new Road(fieldSVG, offsetX, 320, 0);
+    roadPart.draw();
+    offsetX += 100;
+  }
+  n = 3
+  for (let i = 0; i < n; i++) {
+    offsetX = 511
+   
+    const roadPart = new Road(fieldSVG, offsetX, offsetY, 90);
+    roadPart.draw();
+    offsetY += 100;
+  }
+  offsetY = 470
+  for (let i = 0; i < n; i++) {
+    offsetX = 511
+    const roadPart = new Road(fieldSVG, offsetX, offsetY, 90);
+    roadPart.draw();
+    offsetY += 100;
+  }
+
+  
 }
+
+createMap()
+
+
+
+
+
+
+
+
 const roadPath = new Road(fieldSVG); // создание пути для автомобилей
 roadPath.createPath('route1', 'M0 90 L 870 90');
 roadPath.createPath('route2', 'M870 65 L 0 65');
@@ -77,7 +126,7 @@ let checkTimeTraffic = 0
 let checkTime = 0;
 let waitingTime = 5
 let newWaitingTime = 0
-let timeOfCrazyRide = 2
+let timeOfCrazyRide = 1
 let gameInterval;
 let cars = []; // Массив для хранения всех созданных машинок
 let trafficLightsOn = true; // Состояние светофора
@@ -111,14 +160,14 @@ class Auto {
   move() {
     const pathInfo = pathsLengths[this.route];
     let index = cars.indexOf(this);
-    let safeDistance = 25;
+    let safeDistance = 45;
     if (!trafficLightsOn && this.speed === 0 && elapsedTime - elapsedTimeTraffic >= waitingTime && newWaitingTime < timeOfCrazyRide) {
       newWaitingTime = elapsedTime - elapsedTimeTraffic - waitingTime
-      this.speed = this.originalSpeed * 5
+      this.speed = this.originalSpeed
     } else if (!trafficLightsOn && newWaitingTime >= timeOfCrazyRide) {
       elapsedTimeTraffic = elapsedTime
       newWaitingTime = 0
-      this.speed === 0
+      this.speed = 0
     }
 
     if (!trafficLightsOn && this.position >= 430 && this.position < 450 && elapsedTime - elapsedTimeTraffic < waitingTime) {
@@ -198,9 +247,9 @@ function gameTimer() {
   if (elapsedTime - checkTime >= 2) {
     let randomImg = Math.floor(Math.random() * carsImg) + 1;
     let newAuto = new Auto('#route1', '5s', `assets/car${randomImg}.png`, 3, 0).createAuto();
-    let newAuto2 = new Auto('#route2', '5s', `assets/car${randomImg}.png`, 3, 180).createAuto();
+    // let newAuto2 = new Auto('#route2', '5s', `assets/car${randomImg}.png`, 3, 180).createAuto();
     cars.push(newAuto); // Добавляем новую машинку в массив
-    cars.push(newAuto2); // Добавляем новую машинку в массив
+    // cars.push(newAuto2); // Добавляем новую машинку в массив
     checkTime = elapsedTime;
   }
 }
