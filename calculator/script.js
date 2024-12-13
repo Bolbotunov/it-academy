e620f9c438633b51ab008d45566b1b3dbe5984f4
Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2024-12-13 13:49:53 +0300

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,106 +1,109 @@
 function calculator(str) {
-  let stack = []
-  let operation
-  const operations = {
-    '-': 0,
-    '+': 0,
-    '*': 1,
-    '/': 1,
+    let stack = [];
+    let operation;
+    const operations = {
+        '-': 0,
+        '+': 0,
+        '*': 1,
+        '/': 1,
+    };
+    const brackets = {
+        '(': false,
+        ')': true,
+    };
+
+    let completeStack = [];
+    let operatorsStack = [];
+
+    str = str.match(/(\d+(\.\d+)?|\D)/g);
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '-' && (!str[i - 1] || str[i - 1] === '(' || str[i - 1] in operations)) {
+            if (str[i + 1] === '(') {
+                str.splice(i, 1, '-1', '*');
+            } else {
+                str.splice(i, 2, '-' + str[i + 1]);
+            }
+        }
     }
-  const brackets = {
-      '(': false,
-      ')': true,
+
+    str.forEach((item, index) => {
+        if (item in brackets) {
+            if (!brackets[item]) {
+                operatorsStack.push(item);
+            } else {
+                while (operatorsStack.length && operatorsStack[operatorsStack.length - 1] !== '(') {
+                    completeStack.push(operatorsStack.pop());
+                }
+                operatorsStack.pop();
+            }
+        } else if (!(item in operations)) {
+            completeStack.push(item);
+        } else if (item in operations) {
+            while (operatorsStack.length && operations[item] <= operations[operatorsStack[operatorsStack.length - 1]]) {
+                completeStack.push(operatorsStack.pop());
+            }
+            operatorsStack.push(item);
+        }
+    });
+
+    while (operatorsStack.length) {
+        completeStack.push(operatorsStack.pop());
     }
 
-let completeStack = []
-let operatorsStack = []
+    let a;
+    let b;
 
-str = str.match(/(\d+(\.\d+)?|\D)/g)
+    completeStack.forEach((item) => {
+        if (!(item in operations)) {
+            stack.push(Number(item));
+        } else {
+            operation = item;
+            a = Number(stack[stack.length - 2])
+            b = Number(stack[stack.length - 1])
 
-for (let i = 0; i < str.length; i++) {
-  if(str[i] === '-' && (!str[i - 1] || str[i - 1] === '(' || str[i - 1] in operations)) {
-    str.splice(i, 2, '-' + str[i+1])
-    }
-}
+            switch(operation) {
+                case '+':
+                  result = a + b
+                  break;
+                case '-':
+                  result = a - b
+                  break;
+                case '*':
+                  result = a * b
+                  break;
+                case '/':
+                  result = a / b
+                  break;
+                default:
+                  console.log('нет такой операции');
+                  result = null;
+              }
+              stack.pop()
+              stack.pop()
+              stack.push(result)
+            }
+          })
+          return stack.pop()
+          }
 
-str.forEach((item, index) => {
-  if (item in brackets) { 
-    if (!brackets[item]) { 
-      operatorsStack.push(item)
-     } else {
-      while (operatorsStack.length && operatorsStack[operatorsStack.length - 1] !== '(') {
-         completeStack.push(operatorsStack.pop())
-     }
-     operatorsStack.pop()
-     }
-   } else if (!(item in operations)) {
-    completeStack.push(item)
-   } else if (item in operations) {
-      if (operatorsStack.length === 0 || operatorsStack[operatorsStack.length - 1] === '(' ) {
-        operatorsStack.push(item)
-      } else if (operations[item] > operations[operatorsStack[operatorsStack.length - 1]]) {
-        operatorsStack.push(item)
-      } else if(operations[item] <= operations[operatorsStack[operatorsStack.length - 1]]) {
-        completeStack.push(operatorsStack.pop())
-        operatorsStack.push(item)
-      }
-   }
-
-})
-
-while (operatorsStack.length) {
-   completeStack.push(operatorsStack.pop())
+  function test() {
+    console.log(`тест -2: Ожидаемый ответ: -2, Полученный ответ: ${calculator('-2')}`)
+    console.log(`тест -2*-2*(-10*-10-100): Ожидаемый ответ: 0, Полученный ответ: ${calculator('-2*-2*(-10*-10-100)')}`)
+    console.log(`тест -2*-2: Ожидаемый ответ: 4, Полученный ответ: ${calculator('-2*-2')}`)
+    console.log(`тест 5.5+4.2: Ожидаемый ответ: 9.7, Полученный ответ: ${calculator('5.5+4.2')}`)
+    console.log(`тест 100-45.5: Ожидаемый ответ: 54.5, Полученный ответ: ${calculator('100-45.5')}`)
+    console.log(`тест 3*(-4): Ожидаемый ответ: -12, Полученный ответ: ${calculator('3*(-4)')}`)
+    console.log(`тест (-20)/5: Ожидаемый ответ: -4, Полученный ответ: ${calculator('(-20)/5')}`)
+    console.log(`тест (2.5+3.5) * 2: Ожидаемый ответ: 12, Полученный ответ: ${calculator('(2.5+3.5)*2')}`)
+    console.log(`тест 7.1-(-3.1): Ожидаемый ответ: 10.2, Полученный ответ: ${calculator('7.1-(-3.1)')}`)
+    console.log(`тест 9*0.5: Ожидаемый ответ: 4.5, Полученный ответ: ${calculator('9*0.5')}`)
+    console.log(`тест 0-(-10): Ожидаемый ответ: 10, Полученный ответ: ${calculator('0-(-10)')}`)
+    console.log(`тест (8+12) / 4: Ожидаемый ответ: 5, Полученный ответ: ${calculator('(8+12)/4')}`)
+    console.log(`тест -15+(-5): Ожидаемый ответ: -20, Полученный ответ: ${calculator('-15+(-5)')}`)
+    console.log(`тест 2*-(2+2): Ожидаемый ответ: -8, Полученный ответ: ${calculator('2*-(2+2)')}`)
+    console.log(`тест -2*-(2+2): Ожидаемый ответ: 8, Полученный ответ: ${calculator('-2*-(2+2)')}`)
+    console.log(`тест -5*4-(-10+5*(4-5*-1))+5: Ожидаемый ответ: -50, Полученный ответ: ${calculator('-5*4-(-10+5*(4-5*-1))+5')}`)
   }
-  let a
-  let b 
-
-completeStack.forEach((item) => {
-  if (!(item in operations)) {
-    stack.push(Number(item))
-  } else {
-    operation = item
-    a = Number(stack[stack.length - 2])
-    b = Number(stack[stack.length - 1])
-
-    switch(operation) {
-      case '+':
-        result = a + b
-        break;
-      case '-':
-        result = a - b
-        break;
-      case '*':
-        result = a * b
-        break;
-      case '/':
-        result = a / b
-        break;
-      default:
-        console.log('нет такой операции');
-        result = null;
-    }
-    stack.pop()
-    stack.pop()
-    stack.push(result)
-  }
-})
-
-return stack.pop()
-}
-
-function test() {
-  console.log(`тест -2: Ожидаемый ответ: -2, Полученный ответ: ${calculator('-2')}`)
-  console.log(`тест -2*-2*(-10*-10-100): Ожидаемый ответ: 0, Полученный ответ: ${calculator('-2*-2*(-10*-10-100)')}`)
-  console.log(`тест -2*-2: Ожидаемый ответ: 4, Полученный ответ: ${calculator('-2*-2')}`)
-  console.log(`тест 5.5+4.2: Ожидаемый ответ: 9.7, Полученный ответ: ${calculator('5.5+4.2')}`)
-  console.log(`тест 100-45.5: Ожидаемый ответ: 54.5, Полученный ответ: ${calculator('100-45.5')}`)
-  console.log(`тест 3*(-4): Ожидаемый ответ: -12, Полученный ответ: ${calculator('3*(-4)')}`)
-  console.log(`тест (-20)/5: Ожидаемый ответ: -4, Полученный ответ: ${calculator('(-20)/5')}`)
-  console.log(`тест (2.5+3.5) * 2: Ожидаемый ответ: 12, Полученный ответ: ${calculator('(2.5+3.5)*2')}`)
-  console.log(`тест 7.1-(-3.1): Ожидаемый ответ: 10.2, Полученный ответ: ${calculator('7.1-(-3.1)')}`)
-  console.log(`тест 9*0.5: Ожидаемый ответ: 4.5, Полученный ответ: ${calculator('9*0.5')}`)
-  console.log(`тест 0-(-10): Ожидаемый ответ: 10, Полученный ответ: ${calculator('0-(-10)')}`)
-  console.log(`тест (8+12) / 4: Ожидаемый ответ: 5, Полученный ответ: ${calculator('(8+12)/4')}`)
-  console.log(`тест -15+(-5): Ожидаемый ответ: -20, Полученный ответ: ${calculator('-15+(-5)')}`)
-}
-test()
+  test()
+  
